f755a77d157af885f81fd5028df6a3f2ac9b21f8
befovy <befovy@gmail.com>
2019-11-24 00:31:59 +0800

    hugo build

--- all.sh
+++ all.sh
@@ -7,22 +7,14 @@ rm -rf docs
 hugo -d docs
 
 
-# replaceCdn(){
-#     echo "第一个参数为 $1 !"
-#     echo "第二个参数为 $2 !"
-#     echo "第二个参数为 $3 !"
-#     find ./docs -type f -name "*.$1" | xargs sed -i '' "s+$2+$3+g"
-# }
+replaceCdn(){
+    find ./docs -type f -name "*.html" | xargs sed -i '' "s+$1+$2+g"
+    find ./docs -type f -name "*.xml" | xargs sed -i '' "s+$1+$2+g"
+}
 
-# replaceCdn html /manifest.json https://cdn.jsdelivr.net/gh/befovy/blogback@master/docs/manifest.json
-
-find ./docs -type f -name "*.html" | xargs sed -i '' 's+/manifest.json+https://cdn.jsdelivr.net/gh/befovy/blogback@master/docs/manifest.json+g'
-find ./docs -type f -name "*.html" | xargs sed -i '' 's+"/dist/+"https://cdn.jsdelivr.net/gh/befovy/blogback@master/docs/dist/+g'
-find ./docs -type f -name "*.html" | xargs sed -i '' 's+https://gitee.com/befovy/images/raw/master+https://cdn.jsdelivr.net/gh/befovy/images@master+g'
-
-find ./docs -type f -name "*.xml" | xargs sed -i '' 's+/manifest.json+https://cdn.jsdelivr.net/gh/befovy/blogback@master/docs/manifest.json+g'
-find ./docs -type f -name "*.xml" | xargs sed -i '' 's+"/dist/+"https://cdn.jsdelivr.net/gh/befovy/blogback@master/docs/dist/+g'
-find ./docs -type f -name "*.xml" | xargs sed -i ''  's+https://gitee.com/befovy/images/raw/master+https://cdn.jsdelivr.net/gh/befovy/images@master+g'
+replaceCdn /manifest.json https://cdn.jsdelivr.net/gh/befovy/blogback@master/docs/manifest.json
+replaceCdn https://gitee.com/befovy/images/raw/master https://cdn.jsdelivr.net/gh/befovy/images@master
+replaceCdn \"/dist/ \"https://cdn.jsdelivr.net/gh/befovy/blogback@master/docs/dist/
 
 
 hugo serve
@@ -33,4 +25,4 @@ git add -u
 
 git commit -m "hugo build"
 
-# git push
+git push
