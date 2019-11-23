a0e3a9faf6f4a99e751d8c4d49e0d879ab32c8eb
befovy <befovy@gmail.com>
2019-11-24 00:27:39 +0800

    hugo build

--- all.sh
+++ all.sh
@@ -6,13 +6,23 @@ rm -rf docs
 
 hugo -d docs
 
-find ./docs -type f -name "*.html" | xargs sed -i -e  's+/manifest.json+https://cdn.jsdelivr.net/gh/befovy/blogback@master/docs/manifest.json+g'
-find ./docs -type f -name "*.html" | xargs sed -i -e  's+"/dist/+"https://cdn.jsdelivr.net/gh/befovy/blogback@master/docs/dist/+g'
-find ./docs -type f -name "*.html" | xargs sed -i -e  's+https://gitee.com/befovy/images/raw/master+https://cdn.jsdelivr.net/gh/befovy/images@master+g'
 
-find ./docs -type f -name "*.xml" | xargs sed -i -e  's+/manifest.json+https://cdn.jsdelivr.net/gh/befovy/blogback@master/docs/manifest.json+g'
-find ./docs -type f -name "*.xml" | xargs sed -i -e  's+"/dist/+"https://cdn.jsdelivr.net/gh/befovy/blogback@master/docs/dist/+g'
-find ./docs -type f -name "*.xml" | xargs sed -i -e  's+https://gitee.com/befovy/images/raw/master+https://cdn.jsdelivr.net/gh/befovy/images@master+g'
+# replaceCdn(){
+#     echo "第一个参数为 $1 !"
+#     echo "第二个参数为 $2 !"
+#     echo "第二个参数为 $3 !"
+#     find ./docs -type f -name "*.$1" | xargs sed -i '' "s+$2+$3+g"
+# }
+
+# replaceCdn html /manifest.json https://cdn.jsdelivr.net/gh/befovy/blogback@master/docs/manifest.json
+
+find ./docs -type f -name "*.html" | xargs sed -i '' 's+/manifest.json+https://cdn.jsdelivr.net/gh/befovy/blogback@master/docs/manifest.json+g'
+find ./docs -type f -name "*.html" | xargs sed -i '' 's+"/dist/+"https://cdn.jsdelivr.net/gh/befovy/blogback@master/docs/dist/+g'
+find ./docs -type f -name "*.html" | xargs sed -i '' 's+https://gitee.com/befovy/images/raw/master+https://cdn.jsdelivr.net/gh/befovy/images@master+g'
+
+find ./docs -type f -name "*.xml" | xargs sed -i '' 's+/manifest.json+https://cdn.jsdelivr.net/gh/befovy/blogback@master/docs/manifest.json+g'
+find ./docs -type f -name "*.xml" | xargs sed -i '' 's+"/dist/+"https://cdn.jsdelivr.net/gh/befovy/blogback@master/docs/dist/+g'
+find ./docs -type f -name "*.xml" | xargs sed -i ''  's+https://gitee.com/befovy/images/raw/master+https://cdn.jsdelivr.net/gh/befovy/images@master+g'
 
 
 hugo serve
@@ -23,4 +33,4 @@ git add -u
 
 git commit -m "hugo build"
 
-git push
+# git push
