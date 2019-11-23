b126a4a0e933a8f7d7d7c2a404abe71b2197eb66
befovy <befovy@gmail.com>
2019-11-24 00:05:18 +0800

    hugo build

--- all.sh
+++ all.sh
@@ -2,7 +2,18 @@
 
 ../../Projects/mirror/main -config ./mirror.yaml
 
+rm -rf docs
+
 hugo -d docs
+
+find ./docs -type f -name "*.html" | xargs sed -i -e  's+/manifest.json+https://cdn.jsdelivr.net/gh/befovy/blogback@master/docs/manifest.json+g'
+find ./docs -type f -name "*.html" | xargs sed -i -e  's+"/dist/+"https://cdn.jsdelivr.net/gh/befovy/blogback@master/docs/dist/+g'
+find ./docs -type f -name "*.html" | xargs sed -i -e  's+https://gitee.com/befovy/images/raw/master+https://cdn.jsdelivr.net/gh/befovy/images@master+g'
+
+find ./docs -type f -name "*.xml" | xargs sed -i -e  's+/manifest.json+https://cdn.jsdelivr.net/gh/befovy/blogback@master/docs/manifest.json+g'
+find ./docs -type f -name "*.xml" | xargs sed -i -e  's+"/dist/+"https://cdn.jsdelivr.net/gh/befovy/blogback@master/docs/dist/+g'
+find ./docs -type f -name "*.xml" | xargs sed -i -e  's+https://gitee.com/befovy/images/raw/master+https://cdn.jsdelivr.net/gh/befovy/images@master+g'
+
 
 hugo serve
 
