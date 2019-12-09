82e7f02533c33ad0bbe1e0286aa308f63931cfd3
befovy <befovy@gmail.com>
2019-12-09 09:40:39 +0800

    hugo build

--- all.sh
+++ all.sh
@@ -6,25 +6,33 @@ rm -rf docs
 
 hugo -d docs
 
+hugo serve
+
+git status
+
+git add .
+
+git commit -m "hugo build"
+
+hashid=`git rev-parse --short HEAD`
+
+echo $hashid
 
 replaceCdn(){
     find ./docs -type f -name "*.html" | xargs sed -i '' "s+$1+$2+g"
     find ./docs -type f -name "*.xml" | xargs sed -i '' "s+$1+$2+g"
 }
 
-replaceCdn /manifest.json https://cdn.jsdelivr.net/gh/befovy/blogback@master/docs/manifest.json
 replaceCdn https://gitee.com/befovy/images/raw/master https://cdn.jsdelivr.net/gh/befovy/images@master
-replaceCdn \"/dist/ \"https://cdn.jsdelivr.net/gh/befovy/blogback@master/docs/dist/
-replaceCdn /favicon-32x32.png https://cdn.jsdelivr.net/gh/befovy/blogback@master/docs/favicon-32x32.png
-replaceCdn /favicon-16x16.png https://cdn.jsdelivr.net/gh/befovy/blogback@master/docs/favicon-16x16.png
-replaceCdn https://blog.befovy.com/css/ https://cdn.jsdelivr.net/gh/befovy/blogback@master/docs/css/
-replaceCdn https://blog.befovy.com/js/ https://cdn.jsdelivr.net/gh/befovy/blogback@master/docs/js/
-replaceCdn https://blog.befovy.com/images/ https://cdn.jsdelivr.net/gh/befovy/blogback@master/docs/images/
-replaceCdn https:\\\\/\\\\/blog.befovy.com\\\\/\\\\/searchindex.json  https:\\\\/\\\\/cdn.jsdelivr.net\\\\/gh\\\\/befovy\\\\/blogback@master\\\\/docs\\\\/searchindex.json
 
-hugo serve
-
-git status
+replaceCdn /manifest.json https://cdn.jsdelivr.net/gh/befovy/blogback@$hashid/docs/manifest.json
+replaceCdn \"/dist/ \"https://cdn.jsdelivr.net/gh/befovy/blogback@$hashid/docs/dist/
+replaceCdn /favicon-32x32.png https://cdn.jsdelivr.net/gh/befovy/blogback@$hashid/docs/favicon-32x32.png
+replaceCdn /favicon-16x16.png https://cdn.jsdelivr.net/gh/befovy/blogback@$hashid/docs/favicon-16x16.png
+replaceCdn https://blog.befovy.com/css/ https://cdn.jsdelivr.net/gh/befovy/blogback@$hashid/docs/css/
+replaceCdn https://blog.befovy.com/js/ https://cdn.jsdelivr.net/gh/befovy/blogback@$hashid/docs/js/
+replaceCdn https://blog.befovy.com/images/ https://cdn.jsdelivr.net/gh/befovy/blogback@$hashid/docs/images/
+replaceCdn https:\\\\/\\\\/blog.befovy.com\\\\/\\\\/searchindex.json  https:\\\\/\\\\/cdn.jsdelivr.net\\\\/gh\\\\/befovy\\\\/blogback@$hashid\\\\/docs\\\\/searchindex.json
 
 git add .
 
